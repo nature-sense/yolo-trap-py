f994e1f6ac8e4f33286f32caa556230ef765c897
steve <steve@infosense.io>
2025-07-09 12:50:15 +0100

    Corrections yo usb disk mounting

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install -y python3-picamera2 --no-install-recommends
-sudo apt install -y python3-multiprocess
-sudo apt install -y python3-protobuf
-sudo apt install -y python3-numpy
-sudo apt install -y python3-opencv
-sudo apt install -y autofs
-sudo apt install -y git
-sudo apt install -y pmount
-sudo apt install -y python3-tk
+apt update
+apt full-upgrade -y
+apt install -y python3-picamera2 --no-install-recommends
+apt install -y python3-multiprocess
+apt install -y python3-protobuf
+apt install -y python3-numpy
+apt install -y python3-opencv
+apt install -y autofs
+apt install -y git
+apt install -y pmount
+apt install -y python3-tk
 
 python -m venv  --system-site-packages .venv
 source .venv/bin/activate
@@ -17,7 +17,7 @@ echo 'dtoverlay=imx708,cam1' | sudo tee -a /boot/firmware/config.txt
 
 pip install asyncio
 pip install bless
-pip install bleak==0..22.3
+pip install bleak==0.22.3
 pip install json_strong_typing
 pip install ultralytics
 pip install ncnn
@@ -25,11 +25,11 @@ pip install picologging
 pip install thonny
 pip install numpy==1.26.4
 
-sudo mv installation-files/usbstick.rules /etc/udev/rules.d/
-sudo mv installation-files/usbstick-handler@.service /lib/systemd/system/
-sudo mv installation-files/cpmount /usr/local/bin/
-sudo mv installation-files/cpumount /usr/local/bin/
-sudo mv installation-files/yolotrap.service /lib/systemd/system/
+cp installation-files/usbstick.rules /etc/udev/rules.d/
+cp installation-files/usbstick-handler@.service /lib/systemd/system/
+cp installation-files/cpmount /usr/local/bin/
+cp installation-files/cpumount /usr/local/bin/
+cp installation-files/yolotrap.service /lib/systemd/system/
 
 
 
